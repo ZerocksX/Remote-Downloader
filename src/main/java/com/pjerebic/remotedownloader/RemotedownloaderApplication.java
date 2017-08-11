7d88adbcc0ae68144cb3af38d9f70b2ede40721c
Pavao Jerebic <pavao.jerebic@gmail.com>
2017-08-11 16:40:49 +0200

    initial commit
    Signed-off-by: Pavao Jerebic <pavao.jerebic@gmail.com>

---
+++ src/main/java/com/pjerebic/remotedownloader/RemotedownloaderApplication.java
@@ -0,0 +1,12 @@
+package com.pjerebic.remotedownloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemotedownloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemotedownloaderApplication.class, args);
+	}
+}
